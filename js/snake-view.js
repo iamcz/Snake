d5b769d1495fd156f420602225e28b8199457968
Charles Zahn <charlesazahn@gmail.com>
2015-07-03 08:11:50 -0400

    Refactor Snake View.

--- js/snake-view.js
+++ js/snake-view.js
@@ -15,19 +15,15 @@
     var view = this;
     key('left', function() {
       view.moveDir = 'W';
-      //snake.turn('W');
     })
     key('right', function() {
       view.moveDir = 'E';
-      // snake.turn('E');
     })
     key('up', function() {
       view.moveDir = 'S';
-      // snake.turn('S');
     })
     key('down', function() {
       view.moveDir = 'N';
-      // snake.turn('N');
     })
   }
 
@@ -49,51 +45,38 @@
     }.bind(this), 100);
   }
 
+  View.prototype.renderPosWithClass = function(pos, classAttr) {
+    if (pos[0] === null) { return; }
+    var idx = pos[0] + this.board.dimY * pos[1];
+    this.$boardEls.eq(idx).addClass(classAttr);
+  }
+
   View.prototype.render = function () {
     var idx, bodyPart, boardElements = this.$el.find(".board li");
     boardElements.removeClass("snake apple snake-head N S E W");
     
-    bodyPart = this.board.snake.head()
-    idx = this.board.dimY * bodyPart[1] + bodyPart[0];
-    boardElements.eq(idx).addClass("snake-head " + this.board.snake.dir)
-    for (bodyPartIdx in this.board.snake.tail()) {
-      bodyPart = this.board.snake.tail()[bodyPartIdx];
-      if (bodyPart[0] !== null) {
-        idx = this.board.dimY * bodyPart[1] + bodyPart[0];
-        boardElements.eq(idx).addClass("snake");
-      }
-    }
+    var snake = this.board.snake
+    this.renderPosWithClass(snake.head(), "snake-head " + snake.dir)
+
+    snake.tail().forEach( function(tailPos) {
+      this.renderPosWithClass(tailPos, "snake")
+    }.bind(this))
     
-    var applePos = this.board.applePos;
-    idx = this.board.dimY * applePos[1] + applePos[0];
-    boardElements.eq(idx).addClass("apple");
+    this.renderPosWithClass(this.board.applePos, "apple")
   }
   
   View.prototype.setupBoard = function () {
-    var $boardEl = this.$el.find('.board');
-    $boardEl.empty();
+    var $liEl, $boardEl = this.$el.find('.board');
 
-    bodyPart = this.board.snake.head()
-    idx = this.board.dimY * bodyPart[1] + bodyPart[0];
     for (i = 0; i < this.board.dimY; i++){
       for (var j = 0; j < this.board.dimX; j++){
-        var $liEl = $("<li></li>");
-        this.board.snake.tail().forEach(function(bodyPos) {
-          if (SnakeGame.Coord.equals([j,i], bodyPos)){
-            $liEl.addClass("snake");
-          }
-        });
-        if (SnakeGame.Coord.equals([j,i], this.board.snake.head())) {
-          $liEl.addClass("snake-head");
-        }
-
-        if (SnakeGame.Coord.equals([j,i], this.board.applePos)){
-          $liEl.addClass("apple");
-        }
-
+        $liEl = $("<li></li>");
         $boardEl.append($liEl);
       }
     }
+
+    this.$boardEls = this.$el.find(".board li");
+    this.render()
   }
 
 })();
